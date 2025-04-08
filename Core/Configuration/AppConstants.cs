d1759c634ac79ee09a1f94020296e33f99bf3f34
mgrullon <grullonmiguel@gmail.com>
2025-04-08 12:58:28 -0400

    Added New Classes
    Added an application constants class as well as class to store parcel information.

---
+++ Core/Configuration/AppConstants.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection.Emit;
+
+namespace RegridMapper.Core.Configuration
+{
+    public static class AppConstants
+    {
+        public const int DefaultTimeoutSeconds = 10;
+        public const string BaseGoogleUrl = "https://www.google.com/maps/search/";
+        public const string BaseFemaAUrl = "https://msc.fema.gov/portal/search?AddressQuery=";
+        public const string BaseRegridUrlPrefix = "https://app.regrid.com/search?query=";
+        public const string BaseRegridUrlPostfix = "&context=%2Fus&map_id=";
+        public const string BaseHyperlinkFormulaPrefix = "=HYPERLINK(\\";
+    }
+}
---
+++ Models/ParcelData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegridMapper.Models
+{
+    /// <summary>
+    /// Represents parcel data extracted from Regrid.
+    /// </summary>
+    public class ParcelData
+    {
+        public string ParcelID { get; set; }
+        public string Address { get; set; }
+        public string Acres { get; set; }
+        public string OwnerName { get; set; }
+        public string AssessedValue { get; set; }
+        public string ZoningType { get; set; }
+
+        /// <summary>
+        /// Value for the latitude/longitude coordinate system
+        /// Generates the Google Map Url as well as the FEMA Url.
+        /// </summary>
+        public string GeographicCoordinate  { get; set; }
+
+        /// <summary>
+        /// https://www.floodsmart.gov/flood-zones-and-maps
+        /// </summary>
+        public string FloodZone { get; set; }
+
+        // URL values
+        public string FemaUrl { get; set; }
+        public string GoogleUrl { get; set; }
+        public string RegridUrl { get; set; }
+
+        /// <summary>
+        /// Returns a formatted string representing the parcel details.
+        /// </summary>
+        public override string ToString() => $"ParcelID: {ParcelID}, Address: {Address})";
+    }
+}
