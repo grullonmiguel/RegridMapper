d554ff108350839d598110df199b45dc14152c3b
mgrullon <grullonmiguel@gmail.com>
2025-04-08 14:33:52 -0400

    Added Converter/Styles
    Added a Visibility converter as wel as a Styles file

---
+++ Core/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RegridMapper.Converters
+{
+    /// <summary>
+    /// Converts a boolean value to Visibility.
+    /// If the converter parameter is "Inverse", it reverses the logic.
+    /// </summary>
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                bool isInverse = parameter?.ToString()?.Equals("Inverse", StringComparison.OrdinalIgnoreCase) == true;
+                return (boolValue ^ isInverse) ? Visibility.Visible : Visibility.Collapsed;
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+            {
+                bool isInverse = parameter?.ToString()?.Equals("Inverse", StringComparison.OrdinalIgnoreCase) == true;
+                return visibility == Visibility.Visible ^ isInverse;
+            }
+            return false;
+        }
+    }
+}
