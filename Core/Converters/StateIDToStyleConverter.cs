114299b5b75894718d955b28d9952d664964fd72
mgrullon <grullonmiguel@gmail.com>
2025-04-27 18:56:58 -0400

    Code Cleanup
    Cleaned up code for the State Dialog and removed XAML no longer needed

---
+++ Core/Converters/StateIDToStyleConverter.cs
@@ -0,0 +1,23 @@
+﻿using RegridMapper.Core.Configuration;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RegridMapper.Core.Converters
+{
+    public class StateIDToStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is StateCode stateCode)
+            {
+                string styleKey = $"RadioButtons.{stateCode}"; // Convert enum to key
+                return Application.Current.Resources[styleKey] as Style;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) 
+            => throw new NotImplementedException(); // No need for reverse conversion
+    }
+}
